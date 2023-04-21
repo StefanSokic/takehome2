2e2e6e7b24afd75311f5c4d2c8009b039417ac50
Stefan Sokic <stefansokic@Stefans-MacBook-Pro.local>
2023-04-21 14:59:44 -0400

    Added flipper

--- src/components/ShirtFlipper.js
+++ src/components/ShirtFlipper.js
@@ -2,7 +2,26 @@ import React, { useState, useEffect } from 'react';
 
 import '../styles/App.css';
 
-import Shirt1 from '../assets/flipper/shirts/shirts.png';
+import Shirt1 from '../assets/flipper/shirts/shirt1.svg';
+import Shirt1Blue from '../assets/flipper/shirts/shirt1-blue.svg';
+import Shirt2 from '../assets/flipper/shirts/shirt2.svg';
+import Shirt2Blue from '../assets/flipper/shirts/shirt2-blue.svg';
+import Shirt3 from '../assets/flipper/shirts/shirt3.svg';
+import Shirt3Blue from '../assets/flipper/shirts/shirt3-blue.svg';
+import Shirt4 from '../assets/flipper/shirts/shirt4.svg';
+import Shirt4Blue from '../assets/flipper/shirts/shirt4-blue.svg';
+import Shirt5 from '../assets/flipper/shirts/shirt5.svg';
+import Shirt5Blue from '../assets/flipper/shirts/shirt5-blue.svg';
+import Shirt6 from '../assets/flipper/shirts/shirt6.svg';
+import Shirt6Blue from '../assets/flipper/shirts/shirt6-blue.svg';
+import Shirt7 from '../assets/flipper/shirts/shirt7.svg';
+import Shirt7Blue from '../assets/flipper/shirts/shirt7-blue.svg';
+import Shirt8 from '../assets/flipper/shirts/shirt8.svg';
+import Shirt8Blue from '../assets/flipper/shirts/shirt8-blue.svg';
+import Shirt9 from '../assets/flipper/shirts/shirt9.svg';
+import Shirt9Blue from '../assets/flipper/shirts/shirt9-blue.svg';
+import Shirt10 from '../assets/flipper/shirts/shirt10.svg';
+import Shirt10Blue from '../assets/flipper/shirts/shirt10-blue.svg';
 
 import SupremeLogo from '../assets/flipper/logos/supreme-logo.svg';
 import SupremeLogoBlue from '../assets/flipper/logos/supreme-logo-blue.svg';
@@ -26,24 +45,101 @@ import OffWhiteLogo from '../assets/flipper/logos/offwhite-logo.svg';
 import OffWhiteLogoBlue from '../assets/flipper/logos/offwhite-logo-blue.svg';
 
 function ShirtFlipper() {
-    const [count, setCount] = useState(0);
+    const [count, setCount] = useState(9);
+
+    function decementCounter() {
+        if (count < 1) {
+            setCount(9);    
+        } else {
+            setCount(count - 1);
+        }
+    }
 
     useEffect( () => {
         const interval = setTimeout(() => {
-            if (count > 8) {
-                setCount(0);    
-            } else {
-                setCount(count + 1);
-            }
-        }, 1000)
+            decementCounter()
+        }, 1500)
 
         return () => clearInterval(interval);
     }, [count])
+
+    function getClassName(pos) {
+        if (pos == 0) {
+            return "LiftImage ShirtPosition"+pos
+        }
+        return "ShirtPosition"+pos
+    }
+
+    function renderShirts() {
+        let pos1 = (count) % 10
+        let pos2 = (count+1) % 10
+        let pos3 = (count+2) % 10
+        let pos4 = (count+3) % 10
+        let pos5 = (count+4) % 10
+        let pos6 = (count+5) % 10
+        let pos7 = (count+6) % 10
+        let pos8 = (count+7) % 10
+        let pos9 = (count+8) % 10
+        let pos10 = (count+9) % 10
+
+        return <div className='ShirtContainer'>
+            {
+                pos1 === 0  || pos1 === 1
+                ? <img className={getClassName(pos1)} src={Shirt1Blue} alt="shirt1"/>
+                : <img className={getClassName(pos1)} src={Shirt1} alt="shirt1"/>
+            }
+            {
+                pos2 === 0  || pos2 === 1
+                ? <img className={getClassName(pos2)} src={Shirt2Blue} alt="shirt2"/>
+                : <img className={getClassName(pos2)} src={Shirt2} alt="shirt2"/>
+            }
+            {
+                pos3 === 0  || pos3 === 1
+                ? <img className={getClassName(pos3)} src={Shirt3Blue} alt="shirt3"/>
+                : <img className={getClassName(pos3)} src={Shirt3} alt="shirt3"/>
+            }
+            {
+                pos4 === 0  || pos4 === 1
+                ? <img className={getClassName(pos4)} src={Shirt4Blue} alt="shirt4"/>
+                : <img className={getClassName(pos4)} src={Shirt4} alt="shirt4"/>
+            }
+            {
+                pos5 === 0  || pos5 === 1
+                ? <img className={getClassName(pos5)} src={Shirt5Blue} alt="shirt5"/>
+                : <img className={getClassName(pos5)} src={Shirt5} alt="shirt5"/>
+            }
+            {
+                pos6 === 0  || pos6 === 1
+                ? <img className={getClassName(pos6)} src={Shirt6Blue} alt="shirt6"/>
+                : <img className={getClassName(pos6)} src={Shirt6} alt="shirt6"/>
+            }
+            {
+                pos7 === 0  || pos7 === 1
+                ? <img className={getClassName(pos7)} src={Shirt7Blue} alt="shirt7"/>
+                : <img className={getClassName(pos7)} src={Shirt7} alt="shirt7"/>
+            }
+            {
+                pos8 === 0  || pos8 === 1
+                ? <img className={getClassName(pos8)} src={Shirt8Blue} alt="shirt8"/>
+                : <img className={getClassName(pos8)} src={Shirt8} alt="shirt8"/>
+            }
+            {
+                pos9 === 0  || pos9 === 1
+                ? <img className={getClassName(pos9)} src={Shirt9Blue} alt="shirt9"/>
+                : <img className={getClassName(pos9)} src={Shirt9} alt="shirt9"/>
+            }
+            {
+                pos10 === 0  || pos10 === 1
+                ? <img className={getClassName(pos10)} src={Shirt10Blue} alt="shirt10"/>
+                : <img className={getClassName(pos10)} src={Shirt10} alt="shirt10"/>
+            }
+        </div>
+    }
     
     return <div className='ShirtFlip Bordered Shadowed'>
-            <div className='Row'>
+            <div onClick={() => decementCounter()} className='Row'>
                 <div className='ShirtFlipLeft'>
-                    <img src={Shirt1} alt="shirt"/>
+                    {renderShirts()}
                     { 
                         count % 2 == 0 
                             ? <h2 className='ShirtFlipBlue'>CLICK CLICK CLICK</h2>
